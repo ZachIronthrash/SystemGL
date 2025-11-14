6935e1a511254658da381f5f5c0c73f8ebf7b9ab
ZachIronthrash <chriszanehart@gmail.com>
2025-11-13 22:26:21 -0500

    Duplicate models and framerate
    should double check GL_STATIC_DRAW is still what I want

--- SystemGL/Shader.h
+++ SystemGL/Shader.h
@@ -4,6 +4,7 @@
 #define SHADER_H
 
 #include <glad/glad.h> // include glad to get all the required OpenGL headers
+#include <glm/glm.hpp>
 
 #include <string>
 #include <fstream>
@@ -20,12 +21,15 @@ public:
     // constructor reads and builds the shader
     Shader(const char* vertexPath, const char* fragmentPath);
     ~Shader();
+
     // use/activate the shader
     void use();
+
     // utility uniform functions
     void setBool(const std::string& name, bool value) const;
     void setInt(const std::string& name, int value) const;
     void setFloat(const std::string& name, float value) const;
+	void setMat4(const std::string& name, glm::mat4 mat) const;
 };
 
 #endif
